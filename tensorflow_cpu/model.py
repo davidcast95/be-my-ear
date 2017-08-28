c5094fa94738fee6c6cf78bbd5ba50a6cc1a7892
David Wibisono <davidwibisono95@gmail.com>
2017-08-28 21:51:33 +0700

    Now it's ready on Windows with new report text and result csv
    Update to support reported text and result csv with fields [iteration,batch,learning_rate,loss,decode_text]
    bug fix os.path for Windows

--- tensorflow_cpu/model.py
+++ tensorflow_cpu/model.py
@@ -1,10 +1,13 @@
 import os
 import sys
+import csv
 sys.path.append("../")
 from time import gmtime, strftime
 import modules.features.data_representation as data_rep
 import numpy as np
 import tensorflow as tf
+
+csv_fields = ['iteration','batch','learning_rate','ctc_loss','decoded_text']
 
 if len(sys.argv) < 4:
     print ('this method needs 3 args TRAINING_DIR CHECKPOINT_DIR REPORT_DIR')
@@ -12,27 +15,29 @@ if len(sys.argv) < 4:
     print ("CHECKPOINT_DIR ~> directory of model's checkpoint will be stored")
     print ('REPORT_DIR ~> dicretory of result per checkpoint')
 else:
+
+
     #init
     training_dir = sys.argv[1]
     checkpoint_dir = sys.argv[2]
     report_dir = sys.argv[3]
     iteration = 1000
-    batch = 3
+    batch = 1
     num_cep = 26
 
     #property of weight
     mean = 0
     std = 0.3
     relu_clip = 20
-    n_hidden_1 = 128
-    n_hidden_2 = 128
-    n_hidden_3 = 2 * 128
-    n_hidden_4 = 128
-    n_hidden_5 = 128
+    n_hidden_1 = 78
+    n_hidden_2 = 78
+    n_hidden_3 = 2 * 78
+    n_hidden_4 = 78
+    n_hidden_5 = 78
     n_hidden_6 = 28
 
     #property of BiRRN LSTM
-    n_hidden_unit = 1024
+    n_hidden_unit = 8 * 78
     forget_bias = 0
 
     #property of AdamOptimizer (http://arxiv.org/abs/1412.6980) parameters
@@ -49,9 +54,9 @@ else:
     for root, dirs, files in os.walk(training_dir, topdown=False):
         for file in files:
             if file[0] == '_':
-                target_dataset.append(np.load(training_dir + '/' + file))
+                target_dataset.append(np.load(os.path.join(training_dir, file)))
             else:
-                training_dataset.append(np.load(training_dir + '/' + file))
+                training_dataset.append(np.load(os.path.join(training_dir, file)))
 
     training_dataset = data_rep.sparse_dataset(training_dataset)
 
@@ -164,7 +169,7 @@ else:
             print ("Restoring")
             # saving model state
             saver = tf.train.Saver()
-            saver.restore(sess, checkpoint_dir + '/' + last_checkpoint + '/tensorflow_1.ckpt')
+            saver.restore(sess, os.path.join(os.path.join(checkpoint_dir,last_checkpoint),'tensorflow_1.ckpt'))
         else:
             tf.global_variables_initializer().run()
 
@@ -172,16 +177,24 @@ else:
 
         losses = []
         old_losses = []
-        report = open(report_dir + '/report.txt',"a")
+        report = open(os.path.join(report_dir,'report.txt'),"a")
+        reportcsv = open(os.path.join(report_dir,'result.csv'),"a")
+        csvwriter = csv.writer(reportcsv)
         for iter in range(iteration):
+            if iter+last_iteration == 0:
+                csvwriter.writerow(csv_fields)
             print ("iteration #"+str(iter + last_iteration))
             report.write("iteration #"+str(iter + last_iteration))
+            csv_values = []
+            csv_values.append(iter + last_iteration)
             if iter > 0:
                 old_losses = losses
                 losses = []
-            for i in range(len(training_dataset) / batch):
+            for i in range(int(len(training_dataset) / int(batch))):
                 print ("batch #"+str(i))
                 report.write("batch #"+str(i))
+                csv_values.append(i)
+                csv_values.append(learning_rate)
                 #get batch
                 batch_i = training_dataset[(i*batch):(i*batch)+batch]
                 sequence_length = np.array([batch_i.shape[1] for _ in range(batch)])
@@ -201,23 +214,29 @@ else:
                 report.write(decode_text)
 
                 loss = sess.run(avg_loss, feed)
-                print ("negative log-probability :" + loss)
-                report.write("negative log-probability :" + loss)
+                print ("negative log-probability :" + str(loss))
+                report.write("negative log-probability :" + str(loss))
+                csv_values.append(loss)
+                csv_values.append(decode_text)
+                csvwriter.writerow(csv_values)
                 losses.append(loss)
 
                 sess.run(optimizer, feed)
 
             if iter > 0:
+
                 diff = np.array(losses) - np.array(old_losses)
                 th = diff.mean()
+                percentage = th / np.array(old_losses).mean() * 100
                 print ("Learning performance : " + str(th))
                 report.write("Learning performance : " + str(th))
+                report.write("Learning percentage : " + str(percentage))
 
                 if th < threshold:
                     print ("Saving ...")
                     now = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
                     saver = tf.train.Saver()
-                    save_path = saver.save(sess, checkpoint_dir + '/DMC-' + now + '/tensorflow_1.ckpt')
+                    save_path = saver.save(sess, os.path.join(os.path.join(os.path.join(checkpoint_dir, 'DMC-'),now),'tensorflow_1.ckpt'))
                     print ("Checkpoint has been saved on path : " + str(save_path))
                     report.write("Checkpoint has been saved on path : " + str(save_path))
                 else:
@@ -226,12 +245,6 @@ else:
                 print ("Saving ...")
                 now = strftime("%a, %d %b %Y %H:%M:%S", gmtime())
                 saver = tf.train.Saver()
-                save_path = saver.save(sess, checkpoint_dir + '/DMC-' + now + '/tensorflow_1.ckpt')
+                save_path = saver.save(sess, os.path.join(os.path.join(os.path.join(checkpoint_dir, 'DMC-'), now),'tensorflow_1.ckpt'))
                 print ("Checkpoint has been saved on path : " + str(save_path))
                 report.write("Checkpoint has been saved on path : " + str(save_path))
-
-
-
-
-
-
